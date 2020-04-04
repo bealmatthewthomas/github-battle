4de6337ca2ec0b98604c4ca63c61ba40aca75c18
Matthew Beal <matt@butcherbox.com>
2020-04-04 10:13:25 -0400

    added react router

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,29 +1,35 @@
 import React from 'react'
 import propTypes from 'prop-types'
+import { ThemeConsumer} from '../contexts/theme'
 
 export default function Card ({ header, subheader, avatar, href, name, children }){
     return (
-    <div className='card bg-light'>
-        <h4 className='header-lg center-text'>
-            {header}
-          </h4>
-          <img
-            className='avatar'
-            src={avatar}
-            alt={`Avatar for ${name}`}
-          />
-          {subheader && (
-            <h4 className='center-text'>
-                {subheader}
+      <ThemeConsumer>
+        {( {theme} ) => (
+          <div className={`card bg-${theme}`}>
+          <h4 className='header-lg center-text'>
+              {header}
             </h4>
-          )}
-          <h2 className='center-text'>
-            <a className='link' href={href}>
-              {name}
-            </a>
-          </h2>
-          {children}
-    </div>
+            <img
+              className='avatar'
+              src={avatar}
+              alt={`Avatar for ${name}`}
+            />
+            {subheader && (
+              <h4 className='center-text'>
+                  {subheader}
+              </h4>
+            )}
+            <h2 className='center-text'>
+              <a className='link' href={href}>
+                {name}
+              </a>
+            </h2>
+            {children}
+          </div>
+        )}
+      </ThemeConsumer>
+    
     )
 }
 
--- app/index.js
+++ app/index.js
@@ -5,6 +5,7 @@ import Popular from './components/Popular'
 import Battle from './components/Battle'
 import { ThemeProvider } from './contexts/theme'
 import Nav from './components/Nav'
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 
 class App extends React.Component {
@@ -22,14 +23,18 @@ class App extends React.Component {
   }
   render() {
     return (
-      <ThemeProvider value={this.state}>
+    <Router>
+    <ThemeProvider value={this.state}>
         <div className = {this.state.theme}>
           <div className='container'>
             <Nav />
-            <Popular />
+            <Route exact path='/' component ={Popular} />
+            <Route path='/battle' component={Battle}/>
           </div>
         </div>
       </ThemeProvider>
+    </Router>
+      
     )
   }
 }
